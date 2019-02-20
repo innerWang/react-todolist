710620cd8cec90c95f0c639ab0370397548f4883
unknown <52886377@qq.com>
2019-02-20 10:12:04 +0800

    change homepage

---
+++ build/precache-manifest.498c2f1d90e96526dbb45891b8d7d31d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d469c43507b90acef49",
+    "url": "/react-todolist/build/static/js/runtime~main.5d469c43.js"
+  },
+  {
+    "revision": "384b44542cb9c62f12b5",
+    "url": "/react-todolist/build/static/js/main.384b4454.chunk.js"
+  },
+  {
+    "revision": "932a5032563707f72cf9",
+    "url": "/react-todolist/build/static/js/2.932a5032.chunk.js"
+  },
+  {
+    "revision": "384b44542cb9c62f12b5",
+    "url": "/react-todolist/build/static/css/main.3dc34a74.chunk.css"
+  },
+  {
+    "revision": "96ebde2346a665b053f6bd2fdbc464ca",
+    "url": "/react-todolist/build/index.html"
+  }
+];
